5488f1c43c0777692883e2dda4cc3ca032c3b3c7
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2023-04-13 12:05:16 +0200

    fix FR TL issue

--- dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/config/DSSBeanConfig.java
+++ dss-demo-webapp/src/main/java/eu/europa/esig/dss/web/config/DSSBeanConfig.java
@@ -417,6 +417,7 @@ public class DSSBeanConfig {
 		dataLoader.setTimeoutConnectionRequest(connectionRequestTimeout);
 		dataLoader.setRedirectsEnabled(redirectEnabled);
 		dataLoader.setProxyConfig(proxyConfig);
+		dataLoader.setSslProtocol("TLSv1.3");
 		return dataLoader;
 	}
 
